015b19bd6154446319a02c91b5799b2d1f744063
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 17:25:07 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 #define MAX_VALUE_TO_READ 50
 
-void vDisplayBatteryParamters(vector<int> rfSesorReadings)
+// Helper method to display readings on console.
+void vDisplayBatteryParamters(vector<stBatteryParams> rfSesorReadings)
 {
   for(int Index =0; Index<= (int)rfSesorReadings.size(); ++Index)
   {
@@ -13,7 +14,8 @@ void vDisplayBatteryParamters(vector<int> rfSesorReadings)
   }
 }
 
-void vReadBatteryParamtersFromSensor(vector<int> rfSesorReadings)
+// Gets the readings from the sensor.
+void vReadBatteryParamtersFromSensor(vector<stBatteryParams> rfSesorReadings)
 {
   rfSesorReadings.clear();
   for(int ReadNumber =1; ReadNumber<= (int)MAX_VALUE_TO_READ; ++ReadNumber)
@@ -23,9 +25,10 @@ void vReadBatteryParamtersFromSensor(vector<int> rfSesorReadings)
   }
 }
 
+// method reads the values from the sensor and dsiplays the entire values on the console.
 void vGetBatteryParameterValues()
 {
-  vector<int> oSesorReadings;
+  vector<stBatteryParams> oSesorReadings;
   vReadBatteryParamtersFromSensor(oSesorReadings);
   vDisplayBatteryParamters(oSesorReadings);
 }
