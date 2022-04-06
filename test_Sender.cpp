e7fbcd4f7c81436311467aba44644bfbffa1ef46
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 22:38:10 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -1,2 +1,9 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
+#include "Sensor.h"
+#inclde ""Sender.h"
+
+TEST_CASE(" Test the sender to check if it reads all the required values from Sensor") 
+{
+  
+}
