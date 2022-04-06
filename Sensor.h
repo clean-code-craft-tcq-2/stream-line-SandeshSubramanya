f598878d2096e3a3c0df853ebed4cf27d4274184
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 22:57:52 +0530

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -1,5 +1,6 @@
 /*
 This is a simple sensor, that generates combination of 3 values 
+
 1: Internal Temp of the battery
 2: SoC (state of charge)
 3: Charging time in seconds to charge to full battery capacity at this internal temperature
@@ -21,17 +22,5 @@ struct stBatteryParams
   int chargingTimeSec;
 };
 
- void GenerateBatteryParamters(stBatteryParams& rfBatteryParameter)
-{
-  int randomNumber= 1 + (rand() % 50);
-  rfBatteryParameter.internalTemperature = randomNumber;
-  rfBatteryParameter.stateOfCharge = (randomNumber + 10);
-  rfBatteryParameter.chargingTimeSec = (randomNumber * 1000);
-}
-
-stBatteryParams getBatteryParamterValuesFromSensor()
-{
-  stBatteryParams oSingleBatteryParamValue;
-  GenerateBatteryParamters(oSingleBatteryParamValue);
-  return oSingleBatteryParamValue;
-}
+void GenerateBatteryParamters(stBatteryParams& rfBatteryParameter);
+stBatteryParams getBatteryParamterValuesFromSensor();
