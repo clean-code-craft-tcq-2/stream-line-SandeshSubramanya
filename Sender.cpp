60ce9091e1c3ef25e0bc4e4909e3d0db70de8090
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 16:12:05 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -1,1 +1,8 @@
+#pragma once
+#include "Sensor.h"
+using namespace std;
 
+void vDisplayBatteryParamters(stBatteryParams oBatteryParam)
+{
+  cout<<"Temperature: "<<oBatterParameter.internalTemperature<<" SoC: "<<oBatterParameter.stateOfCharge<<" ChargingTime: "<<oBatterParameter.chargingTimeSec<<endl;
+}
