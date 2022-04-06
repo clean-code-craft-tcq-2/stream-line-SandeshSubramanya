e3816caee610516a7de944f81044092598a337e4
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 22:42:58 +0530

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -2,7 +2,11 @@
 This is a simple sensor, that generates combination of 3 values 
 1: Internal Temp of the battery
 2: SoC (state of charge)
-3: Charging time in seconds to charge to full battery capacity at this internal temperatur
+3: Charging time in seconds to charge to full battery capacity at this internal temperature
+
+The logic of generating numbers is simple. It gets a random value in range from 1 to 100 and then adds/multiplies a fixed value
+to generate combination of 3 values.
+It is upto the sender to request as many as sensor values from this sensor.
 */
 
 #pragma once
