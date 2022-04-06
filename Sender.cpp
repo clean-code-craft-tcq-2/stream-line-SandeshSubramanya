43bc47f313f38b4bb7f1cbb230236ae7a2128c90
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 22:34:31 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -1,7 +1,7 @@
 #include "Sender.h"
 
 // Helper method to display readings on console.
-void vDisplayBatteryParamters(vector<stBatteryParams> rfSesorReadings)
+void vDisplayBatteryParamters(vector<stBatteryParams>& rfSesorReadings)
 {
   for(int Index =0; Index<= (int)rfSesorReadings.size(); ++Index)
   {
@@ -10,7 +10,7 @@ void vDisplayBatteryParamters(vector<stBatteryParams> rfSesorReadings)
 }
 
 // Gets the readings from the sensor.
-void vReadBatteryParamtersFromSensor(vector<stBatteryParams> rfSesorReadings)
+void vReadBatteryParamtersFromSensor(vector<stBatteryParams>& rfSesorReadings)
 {
   rfSesorReadings.clear();
   for(int ReadNumber =1; ReadNumber<= (int)MAX_VALUE_TO_READ; ++ReadNumber)
