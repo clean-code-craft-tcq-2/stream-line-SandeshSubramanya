7aae31a060995ebd498a6cd77852846548daa85a
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 22:52:31 +0530

    Update Sender.h

--- Sender.h
+++ Sender.h
@@ -6,11 +6,11 @@ using namespace std;
 #define MAX_VALUE_TO_READ 50
 
 // Helper method to display readings on console.
-void vDisplayBatteryParamters(vector<stBatteryParams>& rfSesorReadings);
+//void vDisplayBatteryParamters(vector<stBatteryParams>& rfSesorReadings);
 
 // Gets the readings from the sensor.
-void vReadBatteryParamtersFromSensor(vector<stBatteryParams>& rfSesorReadings);
+//void vReadBatteryParamtersFromSensor(vector<stBatteryParams>& rfSesorReadings);
 
 // method reads the values from the sensor and dsiplays the entire values on the console.
-void vGetBatteryParameterValues();
+//void vGetBatteryParameterValues();
 
