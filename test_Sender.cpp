3e14e50839595725d58b02d191cafe66105b6d33
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 23:04:10 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -3,7 +3,9 @@
 #include "Sensor.h"
 #include "Sender.h"
 
-TEST_CASE(" Test the sender to check if it reads all the required values from Sensor") 
+TEST_CASE(" Test the usage of Sender") 
 {
-  
+  // The sender is designed such a way that the interface needs to be called.
+  // The sender just displays the various values of battery to the console
+  vReceiveBatteryParameterValues();
 }
