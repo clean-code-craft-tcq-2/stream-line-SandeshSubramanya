7d940d38e4081709414106a4add8f70428ae4e69
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-06 17:16:23 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -1,9 +1,33 @@
 #pragma once
 #include "Sensor.h"
 #include <iostream>
+#include <vector>
 using namespace std;
+#define MAX_VALUE_TO_READ 50
 
-void vDisplayBatteryParamters(stBatteryParams oBatteryParam)
+void vDisplayBatteryParamters(const vector<int> rfSesorReadings)
 {
-  cout<<"Temperature: "<<oBatteryParam.internalTemperature<<" SoC: "<<oBatteryParam.stateOfCharge<<" ChargingTime: "<<oBatteryParam.chargingTimeSec<<endl;
+  for(int Index =0; Index<=rfSesorReadings.size(); ++Index)
+  {
+    cout<<"Temperature: "<<rfSesorReadings[Index].internalTemperature<<" SoC: "<<rfSesorReadings[Index].stateOfCharge<<" ChargingTime: "<<rfSesorReadings[Index].chargingTimeSec<<endl;
+  }
 }
+
+void vReadBatteryParamtersFromSensor(vector<int> rfSesorReadings)
+{
+  rfSesorReadings.clear();
+  for(int ReadNumber =1; ReadNumber<= (int)MAX_VALUE_TO_READ; ++ReadNumber)
+  {
+    stBatteryParams oBatteryParamterValue = getBatteryParamterValuesFromSensor();
+    rfSesorReadings.push_back(oBatteryParamterValue);
+  }
+}
+
+void vGetBatteryParameterValues()
+{
+  vector<int> oSesorReadings;
+  vReadBatteryParamtersFromSensor(oSesorReadings);
+  vDisplayBatteryParamters(oSesorReadings);
+}
+
+
