6d49eba799f143477df0386bb74bf55dff25157f
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-04-07 09:41:08 +0530

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -1,11 +1,11 @@
 /*
-This is a simple sensor, that generates combination of 3 values 
+This is a simple sensor file, that generates combination of 3 values 
 
 1: Internal Temp of the battery
 2: SoC (state of charge)
 3: Charging time in seconds to charge to full battery capacity at this internal temperature
 
-The logic of generating numbers is simple. It gets a random value in range from 1 to 100 and then adds/multiplies a fixed value
+The logic of generating numbers is simple. It initialises a static variable and then adds/multiplies a fixed value
 to generate combination of 3 values.
 It is upto the sender to request as many as sensor values from this sensor.
 */
