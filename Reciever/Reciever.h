58e1b38bb0f1bf88122a8c8e9de3c4528760f2f4
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-04-22 01:21:19 +0530

    Update Reciever.h

--- Reciever/Reciever.h
+++ Reciever/Reciever.h
@@ -1,1 +1,21 @@
+#define STATUS_OK true
+#define STATUS_NOK false
+#define TOTAL_NUMBER_OF_READINGS 50
 
+bool readDataFromConsole(float temperatureReadings[], float stateOfChargeReadings[], float chargingTimeReadings[], int numberOfReadings);
+bool readDataFromFile(float temperatureReadings[], float stateOfChargeReadings[], float chargingTimeReadings[], int numberOfReadings);
+
+float findMaximumReadingValue(float batteryParameterReadings[], int numberOfReadings);
+float findMinimumReadingValue(float batteryParameterReadings[], int numberOfReadings);
+
+float ComputeSimpleMovingAvergeOfLastFiveValues(float batteryParameterReadings[], int numberOfReadings);
+
+void findTemperatureMinAndMaxValue(float temperatureReadings[], int numberOfReadings);
+void findStateOfChargeMinAndMaxValue(float stateOfChargeReadings[], int numberOfReadings);
+void findChargingTimeMinAndMaxValue(float chargingTimeReadings[], int numberOfReadings);
+
+void findTemperatureSimpleMovingAverage(float temperatureReadings[], int numberOfReadings);
+void findStateOfChargeSimpleMovingAverage(float stateOfChargeReadings[], int numberOfReadings);
+void findChargingTimeSimpleMovingAverage(float chargingTimeReadings[], int numberOfReadings);
+
+bool recieveAndCompute(bool(*readDataFunc)(float[],float[],float[],int));
